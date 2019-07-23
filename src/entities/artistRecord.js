4b06513f3644523d9abb654bedcf16c0d84b6e91
Shagun Madhikarmi <madhikarma@gmail.com>
2019-07-23 03:56:14 +0100

    fix: Fix for MySQL db connection pool usage. - Now using the pool to do queries else the connection needed to be released each time once done with else the app requests stall after 10 requests to the db

--- src/entities/artistRecord.js
+++ src/entities/artistRecord.js
@@ -2,27 +2,9 @@ var database = require("../lib/database.js");
 
 getArtists = callback => {
   const connectionPool = database.getConnectionPool();
-  connectionPool.getConnection((connectionError, connection) => {
-    if (connectionError) {
-      console.log(connectionError);
-    }
-    const sql = `SELECT * FROM artists`;
-    connection.query(sql, (error, artists) => {
-      callback(artists, error);
-    });
-  });
-};
-
-getArtist = (id, callback) => {
-  const connectionPool = database.getConnectionPool();
-  connectionPool.getConnection((erroconnectionErrorr, connection) => {
-    if (connectionError) {
-      console.log(connectionError);
-    }
-    const sql = `SELECT * FROM artists WHERE id = ${id}`;
-    connectionPool.query(sql, (error, artists) => {
-      callback(artists[0], error);
-    });
+  const sql = `SELECT * FROM artists`;
+  connectionPool.query(sql, (error, artists) => {
+    callback(artists, error);
   });
 };
 
