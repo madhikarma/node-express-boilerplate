e66af8d6b16d989dab1e4670b7aa6c2f273f9439
Shagun Madhikarmi <madhikarma@gmail.com>
2019-08-05 09:06:01 +0100

    Updated with albums table TODO: add (nested) route

--- scripts/database/create-database.sh
+++ scripts/database/create-database.sh
@@ -5,6 +5,7 @@ MYSQL_USER=$DATABASE_USER
 MYSQL_PASSWORD=$DATABASE_PASSWORD
 MYSQL_PATH="/usr/local/mysql/bin/mysql" # TODO: make this environment aware
 MYSQL_ARTIST_TABLE_NAME="artists"
+MYSQL_ALBUM_TABLE_NAME="albums"
 
 echo "Deleting..."
 "$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "DROP DATABASE $MYSQL_DATABASE"
@@ -14,15 +15,31 @@ echo "Creating database..."
 "$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES"
 
 echo "Creating tables..."
+echo "Creating artist table..."
 "$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE; CREATE TABLE $MYSQL_ARTIST_TABLE_NAME(
-  id int(11) unsigned NOT NULL AUTO_INCREMENT, 
+  id int NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL, 
   PRIMARY KEY (id)
-  );"
+) ENGINE = InnoDB;"
+
+echo "Creating albums table..."
+"$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE; CREATE TABLE $MYSQL_ALBUM_TABLE_NAME(
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL, 
+  artist_id int,
+  FOREIGN KEY (artist_id) REFERENCES artists(id),
+  PRIMARY KEY (id)
+) ENGINE = InnoDB;"
 
 echo "Inserting data..."
+echo "Inserting data into artist table..."
 "$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE; INSERT INTO $MYSQL_ARTIST_TABLE_NAME (name)
 VALUES ('Bonobo');
+"
+
+echo "Inserting data into album table..."
+"$MYSQL_PATH" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE; INSERT INTO $MYSQL_ALBUM_TABLE_NAME (name, artist_id)
+VALUES ('Black Sands', '1');
 "
 
 echo "Finishing up..."
