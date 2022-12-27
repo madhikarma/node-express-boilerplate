9c716246e8447d6955a0e650bfba3c04190721c2
Shagun Madhikarmi <madhikarma@gmail.com>
2022-12-27 07:00:16 +0000

    Adds helper scripts for Docker support

---
+++ scripts/docker-clean-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash scripts/docker-kill.sh
+bash scripts/docker-prune.sh
+bash scripts/docker-build.sh
---
+++ scripts/docker-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -p 49160:8080 -d madhikarma/node-express-boiler-plate:latest
